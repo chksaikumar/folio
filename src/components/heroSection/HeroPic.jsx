b018db2b47766dc844802eec227a7daee7db122e
CHKSAIKUMAR <68428805+chksaikumar@users.noreply.github.com>
2025-05-06 12:45:03 -0500

    Update HeroPic.jsx

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/HexaPic.png"
+        src="https://raw.githubusercontent.com/chksaikumar/folio/refs/heads/main/public/images/HexaPic.png"
         alt="saikumar "
         className="max-h-[450px] w-auto"
       />
